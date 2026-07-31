fcadc62f97f71d7b9ef2fb07df415a3836a660ad
Astha241105 <astha241105@gmail.com>
2026-07-31 20:27:04 +0530

    changed url

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import seatBookingReducer from "../seatbooking/seatBookingSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import seatBookingReducer from "../seatbooking/seatBookingSlice";
 
-// Add other reducers you already have here alongside seatBooking
-// e.g.  import myEventsReducer from "../dashboard_for_host/myEventsSlice";
+// // Add other reducers you already have here alongside seatBooking
+// // e.g.  import myEventsReducer from "../dashboard_for_host/myEventsSlice";
 
-const store = configureStore({
-  reducer: {
-    seatBooking: seatBookingReducer,
-    // myEvents: myEventsReducer,   ← uncomment when you add other slices
-  },
-});
+// const store = configureStore({
+//   reducer: {
+//     seatBooking: seatBookingReducer,
+//     // myEvents: myEventsReducer,   ← uncomment when you add other slices
+//   },
+// });
 
-export default store;
+// export default store;
